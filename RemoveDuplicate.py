27422b9b9eee31302402409c19a7358794eb3eb9
Cyanex1702 <117669695+Cyanex1702@users.noreply.github.com>
2023-06-11 22:12:02 +0500

    Update RemoveDuplicate.py

--- RemoveDuplicate.py
+++ RemoveDuplicate.py
@@ -7,25 +7,27 @@ def remove_duplicates(directory):
     duplicate_images = []
 
     total_images = 0
-    for filename in os.listdir(directory):
-        if filename.endswith(".jpg") or filename.endswith(".png") or filename.endswith(".jpeg"):
-            total_images += 1
+    for root, dirs, files in os.walk(directory):
+        for filename in files:
+            if filename.endswith(".jpg") or filename.endswith(".png") or filename.endswith(".jpeg"):
+                total_images += 1
 
     current_image = 1
-    for filename in os.listdir(directory):
-        if filename.endswith(".jpg") or filename.endswith(".png") or filename.endswith(".jpeg"):
-            image_path = os.path.join(directory, filename)
-            try:
-                image = Image.open(image_path)
-                image_hash = imagehash.average_hash(image)
-                if image_hash in image_hashes:
-                    duplicate_images.append((image_path, image_hashes[image_hash]))
-                else:
-                    image_hashes[image_hash] = image_path
-                print(f"Processed image {current_image}/{total_images}: {image_path}")
-                current_image += 1
-            except Exception as e:
-                print(f"Error processing {image_path}: {e}")
+    for root, dirs, files in os.walk(directory):
+        for filename in files:
+            if filename.endswith(".jpg") or filename.endswith(".png") or filename.endswith(".jpeg"):
+                image_path = os.path.join(root, filename)
+                try:
+                    image = Image.open(image_path)
+                    image_hash = imagehash.average_hash(image)
+                    if image_hash in image_hashes:
+                        duplicate_images.append((image_path, image_hashes[image_hash]))
+                    else:
+                        image_hashes[image_hash] = image_path
+                    print(f"Processed image {current_image}/{total_images}: {image_path}")
+                    current_image += 1
+                except Exception as e:
+                    print(f"Error processing {image_path}: {e}")
 
     for image_path, duplicate_path in duplicate_images:
         os.remove(duplicate_path)
@@ -35,6 +37,6 @@ def remove_duplicates(directory):
     print("Duplicate removal process completed.")
 
 # Provide the path to your image directory
-directory_path = 'PathToDirectory'  # Specify the path to the directory containing images
+directory_path = 'PathtoDirectory'  # Specify the path to the directory containing images
 
 remove_duplicates(directory_path)
