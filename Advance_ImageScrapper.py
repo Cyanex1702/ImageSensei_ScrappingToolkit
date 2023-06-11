cc518e5ceddc43a8e107e714481870c0d21c0715
Cyanex1702 <117669695+Cyanex1702@users.noreply.github.com>
2023-06-11 22:17:49 +0500

    Update Advance_ImageScrapper.py

--- Advance_ImageScrapper.py
+++ Advance_ImageScrapper.py
@@ -3,14 +3,18 @@ from concurrent.futures import ThreadPoolExecutor
 from pathlib import Path
 
 query_list = [
-    # Add your desired queries here
+    # type your query here
+    "human"
 ]
 
 # Remove duplicates from the query_list
 query_list = list(set(query_list))
 
-limit_per_query = 200  # Specify the limit per query
-directory = Path('PathToDataSet')  # Specify the directory where the images will be downloaded
+limit_per_query = 250  # Specify the limit per query
+directory = Path('PathToDirectory')  # Specify the directory where the images will be downloaded
+
+# Create the directory if it doesn't exist
+directory.mkdir(parents=True, exist_ok=True)
 
 def process_query(query):
     output_dir = directory / query
@@ -20,11 +24,11 @@ def process_query(query):
         return  # Skip if the limit is already met
 
     remaining_limit = limit_per_query - downloaded_images
-    downloader.download(query, limit=remaining_limit, output_dir=output_dir, adult_filter_off=False,
+    downloader.download(query, limit=remaining_limit, output_dir=output_dir, adult_filter_off=True,
                         force_replace=False, timeout=60)
 
     # Filter out non-JPEG, JPG, or PNG files
-    [f.unlink() for f in output_dir.glob('*') if f.suffix.lower() not in ('.jpg', '.jpeg', '.png')]
+    [f.unlink() for f in output_dir.glob('*') if f.suffix.lower() not in ('.jpg', '.jpeg')]
 
 # Create a ThreadPoolExecutor with max_workers set to 25
 executor = ThreadPoolExecutor(max_workers=25)
